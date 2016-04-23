2ebe3784cb973fbf514af0b6361c97fde66c6322
Max Gorenflo <max-gorenflo@t-online.de>
2016-04-23 13:36:14 +0200

    Added Search

---
+++ tipuesearch/tipuesearch_content.js
@@ -0,0 +1,10 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Math", "text": "", "tags": "Math", "url": ""},
+     {"title": "Physics", "text": "", "tags": "Physics", "url": ""},
+     {"title": "Material Sciences", "text": "", "tags": "Material Sciences", "url": ""},
+     {"title": "Chemisty", "text": "", "tags": "Chemistry", "url": ""},
+]};
+
+
+
